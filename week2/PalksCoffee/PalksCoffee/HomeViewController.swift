edea27f0cd8758200a8dfaff20e6e3b007782639
youngsun <dudtjs1021@hansung.ac.kr>
2022-04-19 23:43:38 +0900

    [week2] 홈화면 구현

---
+++ week2/PalksCoffee/PalksCoffee/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PalksCoffee
+//
+//  Created by 임영선 on 2022/04/19.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+  @IBOutlet weak var menuView: UIView!
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    menuView.layer.cornerRadius = 10
+  }
+
+
+}
+
