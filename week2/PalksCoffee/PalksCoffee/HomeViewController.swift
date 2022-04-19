605b5ba0f4aaffe33a14deeb6d200acc76eb8ebd
youngsun <dudtjs1021@hansung.ac.kr>
2022-04-20 01:22:42 +0900

    [week2] 홈화면 배너 스크롤뷰 추가

--- week2/PalksCoffee/PalksCoffee/HomeViewController.swift
+++ week2/PalksCoffee/PalksCoffee/HomeViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 class HomeViewController: UIViewController {
 
   @IBOutlet weak var menuView: UIView!
+  @IBOutlet weak var giftshopView: UIView!
+  @IBOutlet weak var smartOrderView: UIView!
+  @IBOutlet weak var eventVView: UIView!
+  @IBOutlet weak var yogiyoView: UIView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     menuView.layer.cornerRadius = 10
+    giftshopView.layer.cornerRadius = 10
+    smartOrderView.layer.cornerRadius = 10
+    eventVView.layer.cornerRadius = 10
+    yogiyoView.layer.cornerRadius = 10
   }
 
 
