325b665c82a2e7e75984fd6bc24697fe34a7f405
youngsun <dudtjs1021@hansung.ac.kr>
2022-04-29 23:33:28 +0900

    [week3] 동네생활 데이터 추가

--- week3/DaangnMarket/DaangnMarket/Controller/Cell/CommunityPostTableViewCell.swift
+++ week3/DaangnMarket/DaangnMarket/Controller/Cell/CommunityPostTableViewCell.swift
@@ -9,15 +9,22 @@ import UIKit
 
 class CommunityPostTableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+  
+  @IBOutlet weak var categoryLabel: UILabel!
+  @IBOutlet weak var postTextView: UITextView!
+  @IBOutlet weak var nicknameLabel: UILabel!
+  @IBOutlet weak var timeLabel: UILabel!
+  @IBOutlet weak var locationLabel: UILabel!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    categoryLabel.layer.cornerRadius = 5
+  }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
-    }
+  }
     
 }
--- week3/DaangnMarket/DaangnMarket/Controller/CommunityViewController.swift
+++ week3/DaangnMarket/DaangnMarket/Controller/CommunityViewController.swift
@@ -12,6 +12,15 @@ class CommunityViewController: UIViewController {
   @IBOutlet weak var postTableView: UITableView!
   @IBOutlet weak var menuCollectionView: UICollectionView!
   let menuArray = ["동네질문", "동네맛집", "동네소식", "취미생활", "분실/실종센터", "동네사건사고", "해주세요", "일상", "고양이", "강아지", "건강", "살림", "동네사진전", "인테리어", "교육/학원", "출산/육아", "기타"]
+  let posts = [
+    PostModel(category: "동네소식", detailText: "마두동에 새로운 와인샵 생겼습니다~~~\n종류도 엄청 많고 가격도 괜찮아요", nickname: "와인좋아", location: "마두동", time: "1분 전"),
+    PostModel(category: "동네사건사고", detailText: "혹시 생수 배송된 집 있나요..?\n저희집에 안왔네요ㅠ", nickname: "나야나", location: "동패동", time: "2분 전"),
+    PostModel(category: "동네질문", detailText: "Q. 일산쪽에 점빼주는 저렴한 곳 있나요?", nickname: "커피향", location: "탄현동", time: "2분 전"),
+    PostModel(category: "동네질문", detailText: "Q. 괜찮게 신발 수선 하는 곳 있나요? ", nickname: "젤리", location: "탄현동", time: "5분 전"),
+    PostModel(category: "분실/실종센터", detailText: "안녕하세요! 탄현쪽에서 걸어가는 길에\n지갑 주우신분 ㅠㅠㅠ", nickname: "아아아아아", location: "일산동", time: "10분 전"),
+    PostModel(category: "기타", detailText: "여기 광고글 엄청 올라오네요\n 광고 아닌 척해도 다 티나고...", nickname: "주주주", location: "마두동", time: "1시간 전"),
+    PostModel(category: "동네맛집", detailText: "일산에서 그릭요거트나 샐러드 맛집 있을까요?? 아니면 스콘같은 디저트도 좋아요", nickname: "러버", location: "덕이동", time: "1시간 전")
+  ]
   override func viewDidLoad() {
     super.viewDidLoad()
     menuCollectionView.register(UINib(nibName: "CommunityMenuCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "communityMenuCell")
@@ -46,7 +55,7 @@ extension CommunityViewController: UITableViewDataSource, UITableViewDelegate {
   }
   
   func numberOfSections(in tableView: UITableView) -> Int {
-    return 5
+    return posts.count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -60,7 +69,12 @@ extension CommunityViewController: UITableViewDataSource, UITableViewDelegate {
       cell.contentView.addSubview(footer)
     }
     
-    
+    let post = posts[indexPath.section]
+    cell.categoryLabel.text = post.category
+    cell.postTextView.text = post.detailText
+    cell.nicknameLabel.text = post.nickname
+    cell.locationLabel.text = post.location
+    cell.timeLabel.text = post.time
     return cell
   }
   
