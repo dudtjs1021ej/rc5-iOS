1e17d1e9546a3007a5971ffcc519fcb07b1a8dfc
youngsun <dudtjs1021@hansung.ac.kr>
2022-05-05 23:44:41 +0900

    [week4] 도구 모드 추가

--- week4/CarrotFarmGame/CarrotFarmGame/Controller/MainViewController.swift
+++ week4/CarrotFarmGame/CarrotFarmGame/Controller/MainViewController.swift
@@ -7,17 +7,52 @@
 
 import UIKit
 
+enum Tool {
+  case wateringCan
+  case pill
+  case hammer
+  case none
+}
+
 class MainViewController: UIViewController {
 
   // MARK: Properties
   @IBOutlet weak var timeLabel: UILabel!
+  @IBOutlet var cropsButtons: [UIButton]!
+  @IBOutlet weak var waterCanButton: UIButton!
+  @IBOutlet weak var pillButton: UIButton!
+  @IBOutlet weak var hammerButton: UIButton!
+  
+  
+  var toolMode: Tool = .none
   var timer: Timer = Timer()
-  var time: Int = 5
+  var time: Int = 1000
+  
   // MARK: - LifeCycle
   override func viewDidLoad() {
     super.viewDidLoad()
     timeLabel.text = String(time)
     createTimer()
+    
+    for button in cropsButtons {
+      button.layer.cornerRadius = 30
+      button.addTarget(self, action: #selector(clcikedCrops), for: .touchUpInside)
+    }
+    
+    waterCanButton.layer.cornerRadius = 40
+    waterCanButton.layer.borderWidth = 2
+    waterCanButton.layer.borderColor = UIColor.clear.cgColor
+    waterCanButton.addTarget(self, action: #selector(clickedWateringCan), for: .touchUpInside)
+    
+    pillButton.layer.cornerRadius = 30
+    pillButton.layer.borderWidth = 2
+    pillButton.layer.borderColor = UIColor.clear.cgColor
+    pillButton.addTarget(self, action: #selector(clickedPill), for: .touchUpInside)
+    
+    hammerButton.layer.cornerRadius = 30
+    hammerButton.layer.borderWidth = 2
+    hammerButton.layer.borderColor = UIColor.clear.cgColor
+    hammerButton.addTarget(self, action: #selector(clickedHammer), for: .touchUpInside)
     
   }
 
@@ -42,5 +77,30 @@ class MainViewController: UIViewController {
       present(vc, animated: false, completion: nil)
     }
   }
+  @objc private func clcikedCrops(_ sender: UIButton) {
+    print("clicked")
+  }
 
+  @objc func clickedPill(_ sender: UIButton) {
+    toolMode = .pill
+    sender.layer.borderColor = UIColor.red.cgColor
+    waterCanButton.layer.borderColor = UIColor.clear.cgColor
+    hammerButton.layer.borderColor = UIColor.clear.cgColor
+    
+  }
+  
+  @objc func clickedWateringCan(_ sender: UIButton) {
+    toolMode = .wateringCan
+    sender.layer.borderColor = UIColor.red.cgColor
+    pillButton.layer.borderColor = UIColor.clear.cgColor
+    hammerButton.layer.borderColor = UIColor.clear.cgColor
+  }
+  
+  @objc func clickedHammer(_ sender: UIButton) {
+    toolMode = .hammer
+    sender.layer.borderColor = UIColor.red.cgColor
+    waterCanButton.layer.borderColor = UIColor.clear.cgColor
+    pillButton.layer.borderColor = UIColor.clear.cgColor
+  }
+  
 }
